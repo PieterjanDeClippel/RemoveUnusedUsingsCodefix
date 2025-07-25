3ba2778dbc95d7f95ecb155e0edec32ca2d20516
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-07-25 15:07:27 +0200

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,8 @@
+﻿// dotnet format ./ConsoleApp19.sln
+// will remove this unused using directive
+
+using System.Text.RegularExpressions;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
